5485e01926de58df9f872ec143809d3ad179e6eb
hehehuai <xihuai87@126.com>
2017-02-03 23:08:42 +0800

    constructor

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -2,7 +2,14 @@ package hellogit;
 
 public class HelloGit {
 
+	/*
+	 * main 
+	 */
 	public static void main(String[] args) {
 		System.out.println("git project!");
+	}
+	
+	public HelloGit() {
+		// TODO Auto-generated constructor stub
 	}
 }
